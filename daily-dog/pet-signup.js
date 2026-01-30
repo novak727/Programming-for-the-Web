16ef18e528b843f6c3afa2a45b930758d6876254
novak727 <devinkennedy727@gmail.com>
2026-01-30 11:49:43 -0600

    Add files via upload

---
+++ daily-dog/pet-signup.js
@@ -0,0 +1,53 @@
+/* Data validation for Daily Dog subscribe form */
+
+function textfield(field) {
+    let txt = document.getElementById(field).value;
+    
+    if (txt.length < 1)
+        return field + " cannot be blank\n";
+    else
+        return "";
+}
+
+function dropdown(field) {
+    let ndx = document.getElementById(field).selectedIndex;
+    
+    if (ndx == -1)
+        return "No " + field + " option selected\n";
+    else
+        return "";
+}
+
+function checkbox(field) {
+    let cbox = document.getElementById(field);
+    
+    if (cbox != null && cbox == true)
+        return "";
+    else
+        return field + " must be checked\n";
+}
+
+function radioset(field) {
+    let rad = document.getElementByName(field);
+    
+    for (i=0; i<rad.length; i++) {
+        if (rad[i].checked != true && rad[i].checked != false)
+            return "No " + field + " radio button selected\n"
+    }
+    
+    return "";
+}
+
+function validate() {
+    let msg = textfield('petname');
+    msg += dropdown('gender');
+    msg += dropdown('food');
+    msg += radioset('age');
+    msg += checkbox('subscribe');
+    
+    if (msg.length > 0) {
+        alert(msg);
+        return false;
+    } else
+        return true;
+}
